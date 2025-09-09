125d2e4f7a55ef969045cc15d60c17eb6d524909
admin <cjlee9029@gmail.com>
2025-09-09 14:38:02 +0900

    first

---
+++ AddToilet.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TM_WPF
+{
+    /// <summary>
+    /// AddToilet.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class AddToilet : Window
+    {
+        private int CardCount = 1;
+        public AddToilet()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+            string floor = FloorTextBox.Text.Trim();
+            string detail = FloorTextBox.Text.Trim();
+            string gender = RadioMale.IsChecked == true ? "남자" : RadioFemale.IsChecked == true ? "여자" : "";
+
+            if (string.IsNullOrEmpty(floor))
+            {
+                MessageBox.Show("층을 입력해주세요.", "입력 확인", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (string.IsNullOrEmpty(gender))
+            {
+                MessageBox.Show("성별을 선택해주세요.", "입력 확인", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if (string.IsNullOrEmpty(detail))
+            {
+                MessageBox.Show("상세 위치를 입력해주세요.", "입력 확인", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            AddCard(floor, gender, detail);
+
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Toilet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TM_WPF
+{
+    internal class Toilet
+    {
+        public string Floor {  get; set; }
+        public string Gender { get; set; }
+        public string LocationDetail {  get; set; }
+        public string CardName => $"{Floor.Substring(0,1)}F_{Gender.Substring(1)}_T_1";
+        public string Status { get; set; }
+    }
+}
